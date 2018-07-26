408efd4599db0831a27e292ee7cb1fd7d8a97588
Rajesh Chinthada <razzesh.java@gmail.com>
2018-07-26 15:42:11 +0530

    complted express router and assignment1

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const express = require('express'),
-     http = require('http');
+http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
 app.use(bodyParser.json());
 
-app.all('/dishes', (req,res,next) => {
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+/*app.all('/dishes', (req,res,next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   next();
@@ -50,21 +75,4 @@ app.put('/dishes/:dishId', (req, res, next) => {
 
 app.delete('/dishes/:dishId', (req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
-});
-
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-
-app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
-
-const server = http.createServer(app);
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
